14f2ed5bc1985793604700e38b5951da5f2c7ff9
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2016-12-27 13:36:43 +0200

    MINOR: Cosmetic improvements

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void print(string s, ostream & os) {
 	os << s;
 }
 
-template <class ... ARGS> void print(std::tuple<ARGS...> t, ostream & os);
+template <class ... Elems> void print(tuple<Elems...> t, ostream & os);
 
 template <class Tuple>
 void print_tuple_impl(Tuple tuple, ostream & os, integer_sequence<size_t>){}
@@ -23,10 +23,10 @@ void print_tuple_impl(Tuple tuple, ostream & os, integer_sequence<size_t, 0, Ind
 	int phony[] = {(os << ",", print(get<Indices>(tuple), os), 0)...};
 }
 
-template <class ... ARGS>
-void print(std::tuple<ARGS...> t, ostream & os) {
+template <class ... Elems>
+void print(tuple<Elems...> t, ostream & os) {
 	os << "[";
-	print_tuple_impl(t, os, std::index_sequence_for<ARGS...>{});
+	print_tuple_impl(t, os, index_sequence_for<Elems...>{});
 	os << "]";
 }
 
